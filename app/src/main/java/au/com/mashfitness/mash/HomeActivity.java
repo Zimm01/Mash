729674cdb6bc655ec3473bb40d477b3c1137fe83
Daniel <d.zimmerman1@hotmail.com>
2018-05-06 09:52:54 +1000

    App Runtime Issues Resolved

--- app/src/main/java/au/com/mashfitness/mash/HomeActivity.java
+++ app/src/main/java/au/com/mashfitness/mash/HomeActivity.java
@@ -24,9 +24,6 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.appbar);
-        setSupportActionBar(toolbar);
-
     }
 
     @Override
