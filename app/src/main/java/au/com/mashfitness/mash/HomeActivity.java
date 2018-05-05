f9793a0161d527763e7cd3649937ce97b73976ce
Daniel <d.zimmerman1@hotmail.com>
2018-05-05 11:50:45 +1000

    Updated HomeScreen Java

--- app/src/main/java/au/com/mashfitness/mash/HomeActivity.java
+++ app/src/main/java/au/com/mashfitness/mash/HomeActivity.java
@@ -27,14 +27,6 @@ public class HomeActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.appbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
