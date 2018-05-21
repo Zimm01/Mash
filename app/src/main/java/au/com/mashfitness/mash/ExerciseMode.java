62d37c7d662a205f00d2b69ca5d4db6d8345a3c5
Liam Patrick Dempsey <liamdempsey100@gmail.com>
2018-05-21 16:48:15 +1000

    added a progress bar to exercise mode

--- app/src/main/java/au/com/mashfitness/mash/ExerciseMode.java
+++ app/src/main/java/au/com/mashfitness/mash/ExerciseMode.java
@@ -1,22 +1,23 @@
 package au.com.mashfitness.mash;
 
 
+
+import android.app.Activity;
 import android.content.Intent;
-import android.support.annotation.MainThread;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import java.util.Timer;
 import java.util.TimerTask;
-import java.util.Date;
-import android.view.WindowManager;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import au.com.mashfitness.mash.list_pages.CustomListPage;
-import au.com.mashfitness.mash.list_pages.SelectExercisePage;
+import android.widget.ProgressBar;
 
 public class ExerciseMode extends AppCompatActivity {
 
@@ -24,13 +25,21 @@ public class ExerciseMode extends AppCompatActivity {
     private boolean timerIsOn = false;
     private boolean updateExercise = false;
     private boolean updateTimer = false;
+    private boolean isCancelled = false;
     static int interval;
     static Timer timer;
+    static Timer progressTimer;
     private String timeOnString;
     private String timeOffString;
     private int counter = 1;
 
+
+    private ProgressBar progress;
+
+
     ImageView exerciseImage;
+    TextView workoutName;
+
 
     private WorkoutStorage exercises = new WorkoutStorage();
 
@@ -50,7 +59,14 @@ public class ExerciseMode extends AppCompatActivity {
         timeOnString = extras.getString("timeOn");
 
 
+
+        TextView setsText = (TextView) findViewById(R.id.infoTextviewID);
+        TextView timeOnText = (TextView) findViewById(R.id.textView);
+        TextView timeOffText = (TextView) findViewById(R.id.textView4);
         exerciseImage = (ImageView) findViewById(R.id.exercise_image);
+        workoutName = (TextView) findViewById(R.id.textView5);
+        progress = findViewById(R.id.progressBar2);
+
 
         //turns time on string to an int
         String[] timeOnValues = timeOnString.split(":");
@@ -68,6 +84,26 @@ public class ExerciseMode extends AppCompatActivity {
         for (String item : customList.split(",")) {
             finalCutomList.add(Integer.parseInt(item));
         }
+        setsText.setText("Sets: "+ Integer.toString(finalCutomList.size()-1));
+        setsText.setTextColor(Color.WHITE);
+        timeOnText.setText("Time On: "+timeOnString);
+        timeOnText.setTextColor(Color.WHITE);
+        timeOffText.setText("Time Off: "+timeOffString);
+        timeOffText.setTextColor(Color.WHITE);
+
+        ImageButton cancelbutton = (ImageButton) findViewById(R.id.imageButton2);
+
+        cancelbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isCancelled = true;
+                timer.cancel();
+                progressTimer.cancel();
+                finish();
+
+            }
+        });
+
 
         //prepares initial setup
         updateExercise(timeOn, timeOff);
@@ -85,13 +121,15 @@ public class ExerciseMode extends AppCompatActivity {
         interval = 1;
         timer.schedule(new TimerTask() {
             public void run() {
-                newThread = new Thread(new ThreadingClass());
-                //starts the workout
-                newThread.run();
-                changeTime(timeOn,timeOff);
-                Log.d("Running new timer", "now");
-                updateExercise(timeOn, timeOff);
-
+                if (isCancelled == false) {
+                    newThread = new Thread(new ThreadingClass());
+                    //starts the workout
+                    newThread.run();
+                    changeTime(timeOn, timeOff);
+                    Log.d("Running new timer", "now");
+                    progressTimer.cancel();
+                    updateExercise(timeOn, timeOff);
+                }
             }
         }, period, delay);
 
@@ -100,18 +138,20 @@ public class ExerciseMode extends AppCompatActivity {
     //this function prepares the custom timer and sends it a time to run for.
     private void changeTime(int timeOn, int timeOff) {
 
-        if (timerIsOn == true) {
-            timer.cancel();
-        }
+        if(isCancelled == false) {
+            if (timerIsOn == true) {
+                timer.cancel();
+            }
 
-        if (updateTimer == false) {
-            newTimer(timeOn, timeOff, timeOn * 1000);
-            updateTimer = true;
-        } else {
-            newTimer(timeOn, timeOff, timeOff * 1000);
-            updateTimer = false;
+            if (updateTimer == false) {
+                newTimer(timeOn, timeOff, timeOn * 1000);
+                updateTimer = true;
+            } else {
+                newTimer(timeOn, timeOff, timeOff * 1000);
+                updateTimer = false;
+            }
+            timerIsOn = true;
         }
-        timerIsOn = true;
     }
 
 
@@ -129,10 +169,14 @@ public class ExerciseMode extends AppCompatActivity {
         }else {
             if (updateExercise == false) {
                 exerciseImage.setImageResource(Integer.parseInt(exercises.getImage(finalCutomList.get(counter))));
+                workoutName.setText(exercises.getName(finalCutomList.get(counter)));
+                startProgressbar(timeOn,timeOff,true);
                 counter += 1;
                 updateExercise = true;
             } else {
                 exerciseImage.setImageResource(Integer.parseInt(exercises.getImage(finalCutomList.get(0))));
+                workoutName.setText(exercises.getName(finalCutomList.get(0)));
+                startProgressbar(timeOn,timeOff,false);
                 updateExercise = false;
             }
         }
@@ -142,6 +186,35 @@ public class ExerciseMode extends AppCompatActivity {
     public void toStatPage() {
         Intent goToActivityBIntent = new Intent(ExerciseMode.this, WorkoutStatPage.class);
         startActivity(goToActivityBIntent);
+
+    }
+
+
+    public void startProgressbar(final int timeOn, final int timeOff, boolean isTimeOn){
+
+
+        if(isTimeOn == true){
+            progress.setMax(timeOn-1);
+        }else{
+            progress.setMax(timeOff-1);
+        }
+        int period = 1000;
+        int delay = 0;
+        //final int period = 1000 * timeOn;
+        progressTimer = new Timer();
+        interval = 0;
+        progressTimer.schedule(new TimerTask() {
+            public void run() {
+
+              progress.setProgress(interval);
+              interval++;
+
+
+            }
+        }, delay, period);
+
+
+
 
     }
 
